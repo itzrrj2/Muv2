6a02337ac1780328486379c8bfce4d23959b1a01
itzrrj2 <providersmmin@gmail.com>
2025-04-28 01:58:18 +0530

    Update info.py

--- info.py
+++ info.py
@@ -20,7 +20,7 @@ BOT_TOKEN = environ.get('BOT_TOKEN', '7854640189:AAEGm3x9NwOLqn1hH49bXH7-Z8yXZhM
 #---------------------------------------------------------------
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '7799707747').split()]
 USERNAME = environ.get('USERNAME', "https://t.me/SR_movies_admin") # ADMIN USERNAME
-LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002545659721'))
+LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002389889873'))
 MOVIE_GROUP_LINK = environ.get('MOVIE_GROUP_LINK', 'https://t.me/SPIDEY_MOVIE_GROUP')
 CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1002339047323').split()]
 #---------------------------------------------------------------
